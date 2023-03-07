7478f880b4661b24b56aa77906f2535e05459273
Lasantha <lasantha1130@gmail.com>
2023-03-07 18:05:47 +0530

    add comments

--- app.js
+++ app.js
@@ -13,17 +13,19 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Create a db for tasks
+// Create a db for tasks in mongo atlas
 mongoose.connect('mongodb+srv://' + process.env.USER + '-admin:' + process.env.PASSWORD + '@cluster0.ctyjgpw.mongodb.net/todolistDB',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
 
+// Table structure for items
 const itemsSchema = new mongoose.Schema({
     name: String
 });
 
+// Create a table for items
 const Item = mongoose.model("Item", itemsSchema);
 
 // Default items to add to an empty List
@@ -41,6 +43,8 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+// Lists for custom task lists
+
 const listSchema = {
     name: String,
     items: [itemsSchema]
@@ -51,14 +55,17 @@ const List = mongoose.model("List", listSchema);
 
 app.get("/", (req, res) => {
 
+    // Mongoose find method
     Item.find({})
         .then((items) => {
+            // If items are empty insert default items and redirect to home route
             if (items.length === 0) {
                 Item.insertMany(defaultItems)
                     .then(() => {
                         console.log("Successfully saved default items to the DB.");
-                        res.redirect("/")
+                        res.redirect("/");
                     })
+                // Else render existing items   
             } else {
                 res.render("list", { listTitle: "Today", newListItems: items });
             }
@@ -67,6 +74,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", function (req, res) {
+    // Get items name and list name from list.ejs
     const itemName = req.body.NewItem;
     const listName = req.body.list;
 
@@ -74,10 +82,12 @@ app.post("/", function (req, res) {
         name: itemName
     });
 
+    //  If list name it 'Today' save it directly
     if (listName === "Today") {
         item.save();
         res.redirect("/");
     } else {
+        // If it is in a different list add it to that 'list.items' array
         List.findOne({ name: listName })
             .then((foundList) => {
                 foundList.items.push(item);
@@ -88,16 +98,21 @@ app.post("/", function (req, res) {
 });
 
 app.post("/delete", (req, res) => {
+    /// Check button will work as a submit button
+    // Get check button status and list name from list.ejs
     const checkedItemId = req.body.checkbox;
     const listName = req.body.listName;
 
+    // If task is in 'Today' list remove it from id
     if (listName === "Today") {
+        // Mongoose remove method by using id
         Item.findByIdAndRemove({ _id: checkedItemId })
             .then((query) => {
                 console.log("Successfully deleted checked item.");
                 res.redirect("/")
             });
     } else {
+        // Else Get the list from lists and remove item from the item array 
         List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } })
             .then((query) => {
                 res.redirect("/" + listName);
@@ -106,12 +121,13 @@ app.post("/delete", (req, res) => {
 });
 
 app.get("/:customListName", (req, res) => {
-
+    // Get custom list name from the url parameters
     let customListName = _.capitalize(req.params.customListName);
 
+    // Mongoose find method for single item
     List.findOne({ name: customListName })
         .then((foundList) => {
-            console.log(foundList);
+            // If list was not found create a new list and add default items to that list
             if (foundList === null) {
                 const list = new List({
                     name: customListName,
@@ -120,6 +136,7 @@ app.get("/:customListName", (req, res) => {
 
                 list.save();
                 res.redirect("/" + customListName);
+                // Else load that found list
             } else {
                 res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
             }
@@ -130,6 +147,7 @@ app.get("/about", function (req, res) {
     res.render("about");
 });
 
+// Port
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
 });
